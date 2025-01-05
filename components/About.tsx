5bd731333cec1256b6fd8a0109eaa58caee6770d
heliosalesjr <65938536+heliosalesjr@users.noreply.github.com>
2025-01-05 19:44:43 -0300

    about the project component

---
+++ components/About.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function About() {
+  return (
+    <>
+      <h1>About this project</h1>
+      <p>This project existes because... </p>
+    </>
+  )
+}
+
+export default About
