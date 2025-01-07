0c5f9c94be7fd0871dc72bbf8e256f99ffbd5b6b
heliosalesjr <65938536+heliosalesjr@users.noreply.github.com>
2025-01-07 19:14:22 -0300

    morphing dialog customization and details

--- components/MorphingDialog.tsx
+++ components/MorphingDialog.tsx
@@ -3,7 +3,8 @@
 import { motion, AnimatePresence } from 'framer-motion'
 import { Button } from '@/components/ui/button'
 import Image from 'next/image'
-import { useDialog } from '../contexts/DialogContext'
+import { useDialog } from '@/contexts/DialogContext'
+import { X } from 'lucide-react'
 
 export function MorphingDialog() {
   const { isDialogOpen, closeDialog } = useDialog()
@@ -20,12 +21,21 @@ export function MorphingDialog() {
           onClick={closeDialog}
         >
           <motion.div
-            className="bg-white dark:bg-gray-800 rounded-lg p-8 max-w-2xl w-full mx-4"
+            className="bg-white dark:bg-gray-800 rounded-lg p-12 max-w-2xl w-full mx-4 relative"
             onClick={(e) => e.stopPropagation()}
           >
+            <Button
+              variant="ghost"
+              size="icon"
+              className="absolute top-2 right-2 text-gray-500 hover:text-gray-700 dark:text-gray-400 dark:hover:text-gray-200"
+              onClick={closeDialog}
+            >
+              <X className="h-6 w-6" />
+              <span className="sr-only">Close</span>
+            </Button>
             <div className="flex flex-col md:flex-row items-center gap-8">
               <Image
-                src="/placeholder.svg?height=200&width=200"
+                src="/me.png"
                 alt="About Image"
                 width={200}
                 height={200}
@@ -42,12 +52,6 @@ export function MorphingDialog() {
                 </p>
               </div>
             </div>
-            <Button
-              onClick={closeDialog}
-              className="mt-6 bg-gradient-to-r from-red-400 to-pink-500 hover:from-red-500 hover:to-pink-600 text-white font-bold py-2 px-4 rounded-full transition duration-300 ease-in-out transform hover:-translate-y-1 hover:scale-110 text-sm"
-            >
-              Close
-            </Button>
           </motion.div>
         </motion.div>
       )}
